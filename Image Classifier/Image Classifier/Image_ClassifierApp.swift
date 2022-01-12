160ebf600dc0e2712f640ac76a17ab28aa858e1f
sai-k02 <sxk180163@utdallas.edu>
2022-01-12 17:46:25 -0600

    initial

---
+++ Image Classifier/Image Classifier/Image_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ClassifierApp.swift
+//  Image Classifier
+//
+//  Created by Srilokh on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Image_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
